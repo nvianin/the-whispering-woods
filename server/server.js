c25ecefb1ae704cb7c675e88615971bfb06615f2
nvianin <n.vianin@gmail.com>
2022-03-14 17:06:00 +0100

    update 5 files

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const log = console.log;
 const io = require("socket.io")();
+const JSONdb = require("simple-json-db")
+const db = new JSONdb("./db.json");
 
 class ForestServer {
     constructor() {
         io.on("connection", client => {
             log(client);
+            client.send()
         })
         io.listen(42069);
         log("Forest server online !")
