c29698757b823078e1ab91d00d5ad2b277c04146
nvianin <n.vianin@gmail.com>
2022-03-16 13:36:46 +0100

    update PlayerController.js and Utils.js

--- classes/PlayerController.js
+++ classes/PlayerController.js
@@ -22,16 +22,20 @@ class PlayerController {
             log(key.key)
             switch (key.key) {
                 case "w":
-                    this.movementForce.z = 1;
-                    break;
-                case "a":
-                    this.movementForce.x = 1;
-                    break;
-                case "s":
                     this.movementForce.z = -1;
                     break;
-                case "d":
+                case "a":
                     this.movementForce.x = -1;
+                    break;
+                case "s":
+                    this.movementForce.z = 1;
+                    break;
+                case "d":
+                    this.movementForce.x = 1;
+                    break;
+
+                case " ":
+                    this.jump();
                     break;
             }
         })
@@ -84,6 +88,7 @@ class PlayerController {
         if (!this.initialized) return false;
         /* log(this.bodies[0].position) */
         this.object.position.set(this.bodies[0].position.x, this.bodies[0].position.y, this.bodies[0].position.z);
+        if (this.bodies[0].sleepState) this.bodies[0].wakeUp()
         this.bodies[0].applyForce(new CANNON.Vec3(this.movementForce.x, this.movementForce.y, this.movementForce.z), new CANNON.Vec3())
         if (this.bodies[0].position.y < -10) this.bodies[0].position.y = 1;
         sun.position.x = this.bodies[0].position.x;
@@ -93,6 +98,17 @@ class PlayerController {
     handleMouseMove(e) {
         this.cameraPivot.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), -e.movementX * .001);
         this.cameraPivot.rotateOnAxis(new THREE.Vector3(1, 0, 0), -e.movementY * .001);
+    }
+
+    jump() {
+        log("jump")
+        raycaster.set(this.bodies[0].position, new THREE.Vector3(0, -1, 0));
+        let i = raycaster.intersectObjects(app.scene.children)
+        /* app.scene.add(new THREE.ArrowHelper(raycaster.ray.origin, raycaster.ray.direction, 300, new THREE.Color(0xff00ff))) */
+        if (i.length > 0 && i[0].distance < 1) {
+            this.bodies[0].applyImpulse(new CANNON.Vec3(0, 5, 0), new CANNON.Vec3())
+        }
+
     }
 
 }
--- classes/Utils.js
+++ classes/Utils.js
@@ -12,3 +12,5 @@ const serializeVector = (v) => {
         z: v.z
     }
 }
+
+let raycaster = new THREE.Raycaster();
